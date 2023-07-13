b98e1a9ca8e2f2f77094322ada53002d8c733cd1
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2023-07-13 12:35:12 -0300

    Created layout of promotions in the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import styles from '@/styles/index.module.css'
+
 import Navbar from '@/components/navbar/navbar' 
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
@@ -17,13 +19,15 @@ export default function Home() {
       <div>
         <Navbar />
         <Container>
-          <div>
+          <div className={styles.session}>
             <Subtitle>Promoções</Subtitle>
-            <div>
+            <div className={styles.salecontainer}>
+              <SaleCard />
+              <SaleCard />
               <SaleCard />
             </div>
           </div>
-          <div>
+          <div className={styles.session}>
             <Subtitle>Outros Jogos</Subtitle>
           </div>
         </Container>
